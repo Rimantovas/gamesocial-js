a68f4e0677cbfd42d367ffdc7002ff487a65113e
Rimantovas <rimas.pov@gmail.com>
2024-05-20 17:26:38 +0300

    Refactor task-related enums, interfaces, and providers

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
-  sourcemap: true,
+  treeshake: true,
+  sourcemap: "inline",
+  minify: true,
   clean: true,
+  dts: true,
+  splitting: false,
+  format: ["cjs", "esm"],
+  external: ["react"],
+  injectStyle: false,
 });
